598a91d262aecf975407901ffaf6968c7f7166e4
iqbalmusyaffa <yusa82401@gmail.com>
2026-09-19 13:50:30 +0700

    update code fix bugs

--- database/migrations/2026_09_13_170500_create_intern_curriculum_progress_table.php
+++ database/migrations/2026_09_13_170500_create_intern_curriculum_progress_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('intern_curriculum_progress');
+
         Schema::create('intern_curriculum_progress', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
